3ace79e2a2c146e22e3466a1ee2788751d7e7c3e
Daucko <dauda.a.tijani@gmail.com>
2025-12-25 09:19:42 +0000

    feat: Implement dashboard layout and register associated routes.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,18 +1,7 @@
-import { auth } from "@/auth";
-import StudentLayout from "./(student-dashboard)/layout";
-import TeacherLayout from "./(teacher-dashboard)/layout";
-
 export default async function UnifiedDashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const session = await auth();
-    const role = (session?.user as any)?.role?.toUpperCase();
-
-    if (role === "STUDENT") {
-        return <StudentLayout>{children}</StudentLayout>;
-    }
-
-    return <TeacherLayout>{children}</TeacherLayout>;
+    return <>{children}</>;
 }
